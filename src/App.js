654457a7860c9632feb09c0299746d44795eb3fd
primocodetoday <primocodetoday@gmail.com>
2020-02-29 20:06:44 +0100

    App initialized from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => (
+  <div className='wrapper'>
+    <h1 className='mainHeader'>Hello world!</h1>
+    <h2 className='secodaryHeader'>Hello papajas!</h2>
+  </div>
+);
 
 export default App;
