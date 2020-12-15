b67e68f2d168e1321fb111ea495719aadb3c9f05
primocodetoday <primocodetoday@gmail.com>
2020-12-15 18:58:44 +0100

    lib: packages update

--- src/components/Header/HeaderNavigation.js
+++ src/components/Header/HeaderNavigation.js
@@ -2,41 +2,26 @@
 import { NavLink } from 'react-router-dom';
 import styles from './HeaderNavigation.module.scss';
 
-const HeaderNavigation = () => {
-  return (
-    <nav>
-      <ul className={styles.wrapper}>
-        <li className={styles.navItem}>
-          <NavLink
-            exact
-            activeClassName={styles.navItemLinkActive}
-            className={styles.navItemLink}
-            to="/"
-          >
-            Tech tubers
-          </NavLink>
-        </li>
-        <li className={styles.navItem}>
-          <NavLink
-            activeClassName={styles.navItemLinkActive}
-            className={styles.navItemLink}
-            to="/articles"
-          >
-            articles
-          </NavLink>
-        </li>
-        <li className={styles.navItem}>
-          <NavLink
-            activeClassName={styles.navItemLinkActive}
-            className={styles.navItemLink}
-            to="/notes"
-          >
-            notes
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+const HeaderNavigation = () => (
+  <nav>
+    <ul className={styles.wrapper}>
+      <li className={styles.navItem}>
+        <NavLink exact activeClassName={styles.navItemLinkActive} className={styles.navItemLink} to="/">
+          Tech tubers
+        </NavLink>
+      </li>
+      <li className={styles.navItem}>
+        <NavLink activeClassName={styles.navItemLinkActive} className={styles.navItemLink} to="/articles">
+          articles
+        </NavLink>
+      </li>
+      <li className={styles.navItem}>
+        <NavLink activeClassName={styles.navItemLinkActive} className={styles.navItemLink} to="/notes">
+          notes
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default HeaderNavigation;
